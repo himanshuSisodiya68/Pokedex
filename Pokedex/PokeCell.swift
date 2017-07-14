d4ace7c146c2ca1e13430e5ed0ef0a2d3ef2679d
Himanshu Sisodiya <himanshu@Himanshu-Sisodiya.local>
2017-07-14 11:35:10 +0530

    removed some images

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,31 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Himanshu Sisodiya on 13/07/17.
+//  Copyright © 2017 Himanshu Sisodiya. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg:UIImageView!
+    @IBOutlet weak var nameLabel:UILabel!
+    
+    var pokemon:Pokemon!
+    
+    //rounded corners for image
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon){
+        self.pokemon = pokemon
+        nameLabel.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+    
+}
